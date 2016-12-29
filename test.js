c03e73d58cc1f7a34f088168cb01fb47cb729614
keke <zhoukekestar@163.com>
2016-12-29 11:09:47 +0800

    Update test examples

--- test.js
+++ test.js
@@ -10,9 +10,15 @@ app.listen(3000)
 console.log('express listen on 3000.')
 
 /*
+
+// 302
 curl -H 'x-forwarded-proto: http' -i  http://localhost:3000/
+curl -H 'x-forwarded-proto: http' -i  http://localhost:3000/test/urla
+curl -H 'x-forwarded-proto: http' -i  http://localhost:3000/test/urla?a=a
+
+// 404
 curl -H 'x-forwarded-proto: https' -i  http://localhost:3000/
 curl -H 'x-forwarded-proto: https' -i  http://localhost:3000/test/url
 curl -H 'x-forwarded-proto: http' -i  http://localhost:3000/test/url
-curl -H 'x-forwarded-proto: http' -i  http://localhost:3000/test/urla
+
 */
